75031748ada5c67c6fb2fde76cd6653025ff468c
SchmidtRichard <ri.schmidta@gmail.com>
2020-09-04 10:46:43 +0100

    Challenge 1 - Render home.ejs

--- app.js
+++ app.js
@@ -8,26 +8,27 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
+//Initialize a new express app
 const app = express();
 
+//Tell the app to use EJS as its view engine
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+//Require body-parser module
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+//Tell the app to use all the statics files inside the public folder
 app.use(express.static("public"));
 
+//Create the first get route on the home route
+app.get("/", function(req, res) {
 
-
-
-
-
-
-
-
-
-
-
-
-
+  //Render the home.ejs file (page)
+  res.render("home");
+});
+//Set up the server to listen to port 3000
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
