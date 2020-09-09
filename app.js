dfabc8209a9e7051e0e1d871b84acbf839df244f
SchmidtRichard <ri.schmidta@gmail.com>
2020-09-09 13:03:48 +0100

    Challenge 11 - Create a global array variable to store the posts - titles and content - and redirect the user back to the home (/) page

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 //Initialize a new express app
@@ -21,6 +23,9 @@ app.use(bodyParser.urlencoded({
 
 //Tell the app to use all the statics files inside the public folder
 app.use(express.static("public"));
+
+//Global variable posts to store all the posts (title and content) in the array
+let posts = [];
 
 //Create the first get route on the home route
 app.get("/", function(req, res) {
@@ -69,9 +74,18 @@ app.post("/compose", function(req, res) {
     content: req.body.postBody
   };
 
+  //Append the posts (title and content) to the posts array (global variable)
+  posts.push(post);
+
+  //Redirect the user back to the home page (/)
+  res.redirect("/");
+
+  console.log("*************** POSTS ARRAY ***************");
+  console.log(posts);
+
   //Log what has been entered in the title and body input in compose.ejs
-  console.log("*************** Post Title ***************\n" + req.body.postTitle);
-  console.log("*************** Post Body ***************\n" + req.body.postBody);
+  console.log("*************** POST TITLE ***************\n" + req.body.postTitle);
+  console.log("*************** POST BODY ***************\n" + req.body.postBody);
 });
 
 //Set up the server to listen to port 3000
